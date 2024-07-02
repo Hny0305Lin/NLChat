4d113a007f0a94ddb3c06a29577f2eb739d395e0
Hny0305Lin <1553809191@qq.com>
2024-07-02 19:10:53 +0800

    添加设置启用显示串口Log

--- app/src/main/java/com/haohanyh/linmengjia/nearlink/nlchat/fun/ChatCore/ChatUtils.java
+++ app/src/main/java/com/haohanyh/linmengjia/nearlink/nlchat/fun/ChatCore/ChatUtils.java
@@ -3,14 +3,19 @@ package com.haohanyh.linmengjia.nearlink.nlchat.fun.ChatCore;
 
 public class ChatUtils {
     //这里设置的是UI相关，是否做到消息滚动展示在UI上，而不是全部展示，全部展示会占用大量UI资源
+    private static boolean showUartLog = false; // 控制是否启用显示串口Log，打印在UI上（开发者使用）
     private static final boolean scrollingMessages = true; // 控制是否启用滚动消息功能
-    private static final boolean clipMessages = true; // 控制是否启用剪贴板功能（目前仅验证码）
+    private static final boolean clipMessages = false; // 控制是否启用剪贴板功能（目前仅验证码）
     //这里设置的是数据库相关
     private static boolean sqlitemanager = true; // 控制是否启用SQLite存储功能
     private static boolean sqlitehistory = false; // 控制是否启用SQLite历史记录显示功能
     //这里设置的是跟C代码相关的，白名单获取聊天文本，当这些文本出现在串口通讯里面的时候，提取这String后者即可，期间过滤掉前者和大量串口log。
     private static final String PREFIX_SERVER = " Let's start chatting, This is the content of the server:";
     private static final String PREFIX_CLIENT = " Let's start chatting, This is the content of the client:";
+    // 控制是否启用显示串口Log，打印在UI上（开发者使用）
+    public static boolean isShowUartLog() { return showUartLog; }
+
+    public static void setShowUartLog(boolean showUartLog) { ChatUtils.showUartLog = showUartLog; }
     //控制是否启用滚动消息功能
     public static boolean isScrollingMessages() { return scrollingMessages; }
     //控制是否启用剪贴板功能和设置
